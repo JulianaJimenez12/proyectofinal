fc099e6832898687e33f3de9c63bff928d65f371
Juliana Jiménez <angiejulianajimenezjimenez@gmail.com>
2025-11-25 15:15:45 -0500

    Add files via upload

---
+++ proyectofinal/proyectointegrador/controlador.js
@@ -0,0 +1,25 @@
+let  boton=document.getElementById("boton")
+boton.addEventListener("click",function(evento){
+    evento.preventDefault() 
+
+    let cajausuariousuario=document.getElementById("cajausuario")
+    let cajacorreocarreo=document.getElementById("cajacorreo")
+    let cajaciudadciudad=document.getElementById("cajaciudad")
+    let cajatelefono=document.getElementById("cajatelefono")
+    let cajacontraseña=document.getElementById("cajacaontraseña")
+
+    let datosDelFormulario={
+        usuario:cajausuario.value,
+        correo:cajacorreo.value,
+        ciudad:cajaciudad.value,
+        telefono:cajatelefono.value,
+        contraseña:cajacontraseña.value,
+    }
+
+    Swal.fire({
+  title: "Bienvenido!",
+  text: "Has sido registrado!",
+  icon: "success"
+});
+    
+})
